2354e0f115ae024983254ec91b4b6473cc845c60
Rafael Campos da Silva <camposdasilva.rafael@gmail.com>
2023-06-30 14:12:18 -0300

    Meu 1º código em C

---
+++ Aula_01/comentarios.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main()
+{
+    /*
+    Tudo entre esses sinais será considerado um comentário.
+    Não importa o número de linhas.
+    */
+
+   // Comentário de uma linha.
+    return 0;
+}
