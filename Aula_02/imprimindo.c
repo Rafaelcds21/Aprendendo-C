2cab9a09dc86659b25ef554a891854f9f6d9545d
Rafael Campos da Silva <camposdasilva.rafael@gmail.com>
2023-06-30 15:15:23 -0300

    Imprimindo valores com printf()

---
+++ Aula_02/imprimindo.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+// Caracter de quebra de linha -> \n, tem que ser inserido dentro das aspas e cada \n representa uma quebra de linha
+
+int main()
+{
+    /*
+    printf é uma função de saída, isso quer dizer que o código mostrará no terminal o que
+    estiver dentro das aspas (simples ou duplas) que estarão dentro dos parênteses.
+    */
+    
+    printf("Hello, World!\n");
+
+    // Todo tipo de caracter entre aspas será impresso.
+    printf("123");
+
+    return 0;
+}
