6095d22738f993ac0b23772ebc5dac95397848bb
Rafael Campos da Silva <camposdasilva.rafael@gmail.com>
2023-07-03 19:16:01 -0300

    Aula 03 adicionada: pausa

---
+++ Aula_03/pausa.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+// system("Pause") ou o que está no main() faz com que o código espere até que o usuário digite um caracter.
+
+int main()
+{
+    printf("Digite qualquer tecla para finalizar o programa.\n");
+    getchar();
+    return 0;
+}
